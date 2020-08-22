a091a90518ec9bdac42ffe735958222369fefc2b
Sherlock Guan <46273832+sherlockguan@users.noreply.github.com>
2020-08-22 22:25:54 +0900

    Update symsum.m

--- ch7 symbolic calculation/symsum.m
+++ ch7 symbolic calculation/symsum.m
@@ -5,3 +5,12 @@ S4 = symsum(k^2,k, 0,1)
 S5 = symsum(k^2,k, 0,n)
 S2 = symsum(1/k^2, k, 1, Inf)
 S3 = symsum(x^k/factorial(k), k, 0, Inf)
+
+
+
+% alternative
+S1 = symsum(k^2, k, [0 10])
+S2 = symsum(1/k^2, k, [1; Inf])
+S3 = symsum(x^k/factorial(k), k, [0 Inf])
+
+%% more on doc sum--on vector[1:10] symsum ect
