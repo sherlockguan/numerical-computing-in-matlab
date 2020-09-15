1bd71dd80ea51d1b0466fcd79f678b77e8a7c302
Sherlock Guan <46273832+sherlockguan@users.noreply.github.com>
2020-09-16 03:31:42 +0900

    Add files via upload

---
+++ char4 plot/complex_plot.m
@@ -0,0 +1,27 @@
+% %https://www.maths.unsw.edu.au/sites/default/files/MatlabSelfPaced/lesson10/MatlabLesson10_Complex.html
+% 
+% % Plot the complex curve z = exp(i* t) for t in [-4, 4]. What should the curve look like?
+% % Answer: 
+% t = linspace(-4,4,401);
+% % % z = exp(complex(0,t)); not working in matlab2018b
+% z = exp(i*t);
+% plot(z);
+% % The plot looks like an ellipse, but should be a circle with centre
+% % the origin and radius 1, as |z| = 1 for all real t. To get a plot 
+% % that looks like a circle, you need to make the axes equal. 
+% 
+% 
+% z1 = 3 + 2*i;
+% % plot(real(z1), imag(z1), '*');
+% % hold on
+% plot(z1, 'o');
+
+
+z2 = [3+2i, -2+1i, -2-1i, 1-2i];
+plot(z2,'*');
+axis([-4 4 -4 4])
+grid on
+xlabel('real part')
+ylabel('imaginary part')
+title('complex number plot')
+gtext(["3+2i"; "-2+1i"; "-2-1i"; "1-2i"])
